6a71c72bb23f6ce71a23e6137c8d1477ac572f2e
JuanDT24 <tiradodj@uninorte.edu.co>
2025-02-14 01:49:08 -0500

    Final touches hopefully

---
+++ go/fib.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func nthFibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return nthFibonacci(n-1) + nthFibonacci(n-2)
+}
+
+func main() {
+	n := 40
+	start := time.Now()
+	result := nthFibonacci(n)
+	elapsed := time.Since(start)
+	fmt.Printf("Time taken: %v seconds\n", elapsed.Seconds())
+}
